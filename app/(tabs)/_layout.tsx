f5dc3e361e8611c0ba987fa6f3d74396de8e6980
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-04-29 12:05:47 +0100

    updated tabs layout with new tabs, and changed styles for tab bar. Icons are yet to be updated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,74 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { useTheme } from "@/hooks/use-theme";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
+  const { palette, colorScheme } = useTheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: palette.tabIconSelected,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: colorScheme === "light" ? "#FFFFFFA6" : "#0E190E",
+          backdropFilter: "blur(20px)",
+          borderTopWidth: 1,
+          borderTopColor: colorScheme === "light" ? "#FFFFFF80" : "#14141E99",
+          elevation: 0,
+          position: "absolute",
+          height: 50,
+          borderRadius: 40,
+          marginBottom: 40,
+          marginLeft: 30,
+          marginRight: 30,
+          paddingTop: 4,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol
+              size={28}
+              name="house.fill"
+              color={palette.tabIconDefault}
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="plantas"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Plantas",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol
+              size={28}
+              name="paperplane.fill"
+              color={palette.tabIconDefault}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: "Mapa",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol
+              size={28}
+              name="paperplane.fill"
+              color={palette.tabIconDefault}
+            />
+          ),
         }}
       />
     </Tabs>
