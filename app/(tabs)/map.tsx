bf61fb9f9f1e010a64a774c4d5b05736acec2304
Maria Luque Larranaga <marialuquelcp@gmail.com>
2026-05-22 15:09:20 +0100

    developed map screen. On load, map renders on specific region and displays the users plants via markers on the map

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,20 +1,77 @@
-import { StyleSheet } from "react-native";
+import { useEffect } from "react";
+import { Image, StyleSheet } from "react-native";
 
-import { GradientBackground } from "@/components/GradientBackground";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import axios from "axios";
+import { useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+interface PlantLocation {
+  latitude: number;
+  longitude: number;
+}
 
 export default function Map() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [markers, setMarkers] = useState<PlantLocation[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function getLocations() {
+      try {
+        const { data } = await axios<any>(
+          "https://bosque-comestible-backend.onrender.com/api/plants/locations",
+        );
+        const { locations } = data;
+        setMarkers(locations);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getLocations();
+  }, []);
+
   return (
     <SafeAreaProvider>
-      <GradientBackground>
-        <SafeAreaView style={styles.safeArea}></SafeAreaView>
-      </GradientBackground>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 38.13108,
+          longitude: -4.776098,
+          latitudeDelta: 0.002,
+          longitudeDelta: 0.001,
+        }}
+        mapType="satellite"
+      >
+        {markers.map((marker) => {
+          return (
+            <Marker
+              key={markers.indexOf(marker)}
+              coordinate={{
+                latitude: marker.latitude,
+                longitude: marker.longitude,
+              }}
+            >
+              <Image
+                source={require("@/assets/images/tree_marker.png")}
+                style={styles.marker}
+              />
+            </Marker>
+          );
+        })}
+      </MapView>
     </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
+  map: {
     flex: 1,
+  },
+  marker: {
+    width: 30,
+    height: 30,
   },
 });
